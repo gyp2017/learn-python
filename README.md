0d733ad85182705a6329f2bdab341286e8967fc2
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-04 17:12:23 +0900

    add 00, 01

---
+++ README.md
@@ -0,0 +1,3 @@
+# 깐깐하게 배우는 파이썬
+
+> Learn PYTHON the HARD WAY 3rd edition
---
+++ src/01/ex1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+# 주석
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+print "안녕 파이썬!"
