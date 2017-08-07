83673d0eaa286aa78e34d7a5640b78e359fc0e4d
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-07 21:31:45 +0900

    add 03

--- src/01/ex1.py
+++ src/01/ex1.py
@@ -2,10 +2,9 @@
 
 print "Hello World!"
 print "Hello Again"
-# 주석
-# print "I like typing this."
-# print "This is fun."
-# print 'Yay! Printing.'
-# print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.'
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
 print "안녕 파이썬!"
