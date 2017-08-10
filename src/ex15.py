7a63cfaf788f55f1b805d10a570c44a324de47de
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-10 20:25:48 +0900

    add ex15

---
+++ src/ex15.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# 15 파일 읽기
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "파일 %r의 내용:" % filename
+print txt.read()
+
+print "파일 이름을 다시 입력해 주세요."
+file_again = raw_input(">")
+
+txt_again = open(file_again)
+
+print txt_again.read()
