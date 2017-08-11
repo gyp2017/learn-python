43ffc07eb54a990b7f740c583312f4781464b46c
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-11 22:19:10 +0900

    add ex18

--- src/ex17-1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# 17 파일 더 다뤄 보기
-
-from sys import argv
-from os.path import exists
-
-script, from_file, to_file = argv
-
-open(to_file, 'w').write(open(from_file, 'r').read())
