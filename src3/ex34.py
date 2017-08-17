3c371e1eb2700b0fa95acc2777d0a272a189c7a5
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-17 15:10:22 +0900

    add ex34

---
+++ src3/ex34.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 34 리스트 원소 접근
+
+animals = ['곰', '비단뱀', '공작', '캥거루', '고래', '오리너구리']
+
+print "1. 1에 있는 동물: %s" % animals[1]
+print "2. 세 번째 동물: %s" % animals[2]
+print "3. 첫 번째 동물: %s" % animals[0]
+print "4. 3에 있는 동물: %s" % animals[3]
+print "5. 다섯 번째 동물: %s" % animals[4]
+print "6. 2에 있는 동물: %s" % animals[2]
+print "7. 여섯 번째 동물: %s" % animals[5]
+print "8. 4에 있는 동물: %s" % animals[4]
