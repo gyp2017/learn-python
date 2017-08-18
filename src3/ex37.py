d02d967af531c22a3f2d7346620f5ff6a95a0a32
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-18 13:31:56 +0900

    add ex37

---
+++ src3/ex37.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# 37 기호 점검
+
+print "%d" % 45
+print "%i" % 45
+print "%o" % 1000
+print "%u" % -1000
+print "%x" % 1000
+print "%X" % 1000
+print "%e" % 1000
+print "%E" % 1000
+print "%f" % 10.34
+print "%F" % 10.34
+print "%g" % 10.34
+print "%G" % 10.34
+print "%c" % 34
+print "%r" % int
+print "%s there" % 'hi'
+print "%g%%" % 10.34
