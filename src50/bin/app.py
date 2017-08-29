5b7056f1bc93433e068a09a715fa94efac011fac
Gim YoungPhil <youngphil.gim@gmail.com>
2017-08-29 19:42:30 +0900

    add ex51

--- src50/bin/app.py
+++ src50/bin/app.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 # 50 처음 만드는 웹사이트
+# 51 웹 브라우저에서 입력 받기
 
 import web
 
 urls = (
-    '/', 'index'
+    '/hello', 'index'
 )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
-class index:
+class index(object):
     def GET(self):
-          greeting = "Hello World"
-          return render.index(greeting = greeting)
+        return render.hello_form_laid_out()
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+
+        return render.index_laid_out(greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
